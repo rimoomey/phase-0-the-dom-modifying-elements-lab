80dac0b738c5e5883bb49438ff7ded6347a3d664
Rimon Moomey <rimon.moomey@gmail.com>
2022-10-14 14:15:20 -0500

    complete JS exercise

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.querySelector("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "is the champion";
